f1562f6275d87d36886313765de3397a7f058564
MUTED64 <muted32555664@gmail.com>
2023-07-11 13:57:25 +0800

    Phoneme duration adjust version 1.0

--- PhonemeDurationAdjuster.js
+++ PhonemeDurationAdjuster.js
@@ -2,8 +2,8 @@ function getClientInfo() {
   return {
     name: SV.T("Phoneme Duration Batch Adjustment"),
     author: "MUTED64",
-    versionNumber: 0.1,
-    minEditorVersion: 65540,
+    versionNumber: 1.0,
+    minEditorVersion: 65537,
   };
 }
 
@@ -13,27 +13,39 @@ function getTranslations(langCode) {
       ["Phoneme Duration Batch Adjustment", "音素长度批量调节"],
       ["No note selected", "未选择音符"],
       ["Please select some notes first.", "请先选中一些音符。"],
-      ["Current Voice Language", "当前歌声语言"],
+      ["Current Singing Language", "当前歌声语言"],
       ["Cantonese", "粤语"],
       ["English", "英语"],
       ["Japanese", "日语"],
       ["Mandarin", "普通话"],
       ["Vowels Duration", "元音长度"],
       ["Consonants Duration", "辅音长度"],
-      ["Please select the current singing language and select the vowel and consonant length.", "请选择当前歌唱语言，并选择需要的元音和辅音长度。"]
+      [
+        "Please select the current singing language and select the vowel and consonant length.",
+        "请选择当前歌唱语言，并选择需要的元音和辅音长度。",
+      ],
     ];
   }
   return [];
 }
 
+function isVowel(phoneme, phonemeDict) {
+  return phonemeDict.vowels.indexOf(phoneme) !== -1;
+}
+
+function isConsonant(phoneme, phonemeDict) {
+  return phonemeDict.consonants.indexOf(phoneme) !== -1;
+}
+
 function main() {
-  var selection = SV.getMainEditor().getSelection();
+  var editorView = SV.getMainEditor();
+  var selection = editorView.getSelection();
   var selectedNotes = selection.getSelectedNotes();
-  var scope = SV.getMainEditor().getCurrentGroup();
-  var group = scope.getTarget();
+  var currentGroupRef = editorView.getCurrentGroup();
 
   var phonemeCategories = [
-    { // Cantonese
+    {
+      // Cantonese
       vowels: [
         "a",
         "6",
@@ -81,7 +93,8 @@ function main() {
         "kwh",
       ],
     },
-    { // English
+    {
+      // English
       vowels: [
         "aa",
         "ae",
@@ -130,10 +143,10 @@ function main() {
         "zh",
       ],
     },
-    { // Japanese
+    {
+      // Japanese
       vowels: ["a", "i", "u", "e", "o", "N"],
       consonants: [
-        "cl",
         "t",
         "d",
         "s",
@@ -166,7 +179,8 @@ function main() {
         "y",
       ],
     },
-    { // Mandarin
+    {
+      // Mandarin
       vowels: [
         "a",
         "A",
@@ -244,41 +258,80 @@ function main() {
       buttons: "OkCancel",
       widgets: [
         {
-          "name" : "currentLanguage", 
-          "type" : "ComboBox",
-          "label" : SV.T("Current Singing Language"),
-          "choices" : [SV.T("Cantonese"), SV.T("English"), SV.T("Japanese"), SV.T("Mandarin")],
-          "default" : 2,
+          name: "currentLanguage",
+          type: "ComboBox",
+          label: SV.T("Current Singing Language"),
+          choices: [
+            SV.T("Cantonese"),
+            SV.T("English"),
+            SV.T("Japanese"),
+            SV.T("Mandarin"),
+          ],
+          default: 2,
         },
         {
-          "name" : "consonantsDuration", 
-          "type" : "Slider",
-          "label" : SV.T("Consonants Duration"),
-          "format" : "%.2f",
-          "minValue" : 0.2,
-          "maxValue" : 1.8,
-          "interval" : 0.05,
-          "default" : 1
+          name: "consonantsDuration",
+          type: "Slider",
+          label: SV.T("Consonants Duration"),
+          format: "%.2f",
+          minValue: 0.2,
+          maxValue: 1.8,
+          interval: 0.05,
+          default: 1,
         },
         {
-          "name" : "vowelsDuration",
-          "type" : "Slider",
-          "label" : SV.T("Vowels Duration"),
-          "format" : "%.2f",
-          "minValue" : 0.2,
-          "maxValue" : 1.8,
-          "interval" : 0.05,
-          "default" : 1
+          name: "vowelsDuration",
+          type: "Slider",
+          label: SV.T("Vowels Duration"),
+          format: "%.2f",
+          minValue: 0.2,
+          maxValue: 1.8,
+          interval: 0.05,
+          default: 1,
         },
       ],
     };
 
     var result = SV.showCustomDialog(form);
-    // object to string
-    // SV.showMessageBox("result", JSON.stringify(result));
 
-    if(result.status){
-      // TODO
+    if (result.status) {
+      var currentLanguage = result.answers.currentLanguage;
+      var consonantsDuration = Number(
+        result.answers.consonantsDuration
+      ).toFixed(2);
+      var vowelsDuration = Number(result.answers.vowelsDuration).toFixed(2);
+      var phonemeDict = phonemeCategories[currentLanguage];
+      var groupPhonemes = SV.getPhonemesForGroup(currentGroupRef);
+
+      var phonemes = [];
+      for (var i = 0; i < selectedNotes.length; i++) {
+        var noteIndex = selectedNotes[i].getIndexInParent();
+        phonemes.push(groupPhonemes[noteIndex]);
+      }
+
+      for (var i = 0; i < selectedNotes.length; i++) {
+        var currentNote = selectedNotes[i];
+        var noteIndex = currentNote.getIndexInParent();
+        var currentNotePhonemes =
+          currentNote.getPhonemes() !== ""
+            ? currentNote.getPhonemes()
+            : groupPhonemes[noteIndex];
+        currentNotePhonemes = currentNotePhonemes.split(" ");
+        var phonemeDuration = []; // 0: vowel, 1: consonant, 2: others
+        for (var j = 0; j < currentNotePhonemes.length; j++) {
+          if (isVowel(currentNotePhonemes[j], phonemeDict)) {
+            phonemeDuration.push(vowelsDuration);
+          } else if (isConsonant(currentNotePhonemes[j], phonemeDict)) {
+            phonemeDuration.push(consonantsDuration);
+          } else {
+            phonemeDuration.push(1);
+          }
+        }
+
+        currentNote.setAttributes({
+          dur: phonemeDuration,
+        });
+      }
     }
     SV.finish();
   }
